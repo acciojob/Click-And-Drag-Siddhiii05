0e5e69b3e5dc5d97201ad9452bd1ecf13870d409
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-27 18:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Your code here.
 const slider = document.querySelector(".items");
 
 let isDown = false;
@@ -8,7 +7,7 @@ let scrollLeft;
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
 });
 
@@ -26,7 +25,6 @@ slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
 
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; // Scroll speed
+  const walk = (e.pageX - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
 });
