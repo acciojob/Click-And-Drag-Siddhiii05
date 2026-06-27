117d3fc461993390a2422c4f91aa10a983042494
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-27 18:47:31 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector(".items");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const walk = (startX - e.pageX) * 3;
+  slider.scrollLeft = scrollLeft + walk;
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
